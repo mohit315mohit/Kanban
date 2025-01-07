3f2872f18d5aa2590cdba9f3c7f1bdd4b3e5147d
Mohit Kumar <mohitpundir099@gmail.com>
2025-01-07 10:48:23 +0530

    Your commit message

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css'
 import App from './App.jsx'
 
+const approutes =createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: < />,
+    },
+    {
+      path: '/about/contact',
+      element: <ContactPage />,
+    },
+  ]
+)
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
